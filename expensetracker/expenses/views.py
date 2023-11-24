7a477c62b1c61b059a0b6e5bd9c09f9997df3d31
IsraelRobTheProgrammer <israelrobinson965@gmail.com>
2023-11-24 18:46:46 +0100

    feature: added pagination functionality

--- expensetracker/expenses/views.py
+++ expensetracker/expenses/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from .models import Category, Expenses
 from django.contrib import messages
+from django.core.paginator import Paginator
 
 # Create your views here.
 
@@ -11,7 +12,13 @@ def index(request):
     categories = Category.objects.all()
     expenses = Expenses.objects.filter(owner=request.user)
 
-    context = {"expenses": expenses}
+    paginator = Paginator(expenses, 1)
+    print()
+    page_number = request.GET.get("page")
+    page_obj = paginator.get_page(page_number)
+    print()
+
+    context = {"expenses": expenses, "page_obj": page_obj}
     return render(request, "expenses/index.html", context)
 
 
