36cb1dcae5dc43df05e6449c5631f092cc747aef
IsraelRobTheProgrammer <israelrobinson965@gmail.com>
2025-08-06 13:23:31 +0100

    Deploy changes: these changes made should suffice for the changes

---
+++ README.md
@@ -0,0 +1,50 @@
+EXPENSES-TRACKER
+			Built with the tools and technologies:
+JavaScript, Python, HTML, CSS														
+
+
+Overview
+
+Expenses-Tracker is a comprehensive, open-source Django project designed to simplify the development of personal finance management applications. It offers a modular architecture with core features for expense and income tracking, user preferences, and secure authentication
+
+Getting Started
+
+Prerequisites
+
+This project requires the following dependencies:
+
+Programming Language: Python
+
+Package Manager: Pip
+
+
+Installation
+
+Build Expenses-Tracker from the source and install dependencies:
+
+4.	Clone the repository:
+
+
+❯	git clone https://github.com/IsraelRobTheProgrammer/Expenses-Tracker
+
+
+
+5.	Install the dependencies:
+
+Using pip: 
+
+❯ pip install -r requirements.txt
+
+
+6.	Navigate to the project directory:
+
+
+❯	cd expensetracker
+
+
+USAGE
+7. Run the project with:
+
+Using pip:
+
+python manage.py runserver
--- expensetracker/expensetracker/settings.py
+++ expensetracker/expensetracker/settings.py
@@ -27,7 +27,7 @@ environ.Env.read_env(os.path.join(BASE_DIR, ".env"))
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = env("SECRET_KEY")
+SECRET_KEY = env("DJANGO_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -85,27 +85,27 @@ WSGI_APPLICATION = "expensetracker.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
-    "default": dj_database_url.config(
-        # Replace this value with your local database's connection string.
-        default="postgresql://postgres:postgres@localhost:5432/expensetracker",
-        conn_max_age=600,
-    )
-}
 # DATABASES = {
-#     "default": {
-#         "ENGINE": "django.db.backends.postgresql",
-#         "NAME": env("DB_NAME"),
-#         "USER": env("DB_USER"),
-#         "PASSWORD": env("DB_PASSWORD"),
-#         "HOST": env("DB_HOST"),
-#         "PORT": env("DB_PORT"),
-#         "OPTIONS": {
-#             "sslmode": "require",  # or 'verify-ca', 'verify-full'
-#             # "channel_binding":"require"
-#         },
-#     }
+#     "default": dj_database_url.config(
+#         # Replace this value with your local database's connection string.
+#         default="postgresql://postgres:postgres@localhost:5432/expensetracker",
+#         conn_max_age=600,
+#     )
 # }
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": env("DB_NAME"),
+        "USER": env("DB_USER"),
+        "PASSWORD": env("DB_PASSWORD"),
+        "HOST": env("DB_HOST"),
+        "PORT": env("DB_PORT"),
+        "OPTIONS": {
+            # "sslmode": "require",  # or 'verify-ca', 'verify-full'
+            # "channel_binding":"require"
+        },
+    }
+}
 
 
 # Password validation
